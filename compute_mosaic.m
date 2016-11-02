cf9e7bdfa7ab4ff3f92d42356e38391bf56e1964
frankpuppa <fdellapuppa@dundee.ac.uk>
2016-11-02 21:59:18 +0000

    removed matlab

--- compute_mosaic.m
+++
@@ -1,7 +0,0 @@
-function compute_mosaic( num_tiles, filename )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-    Image=imread(filename); 
-    figure,imshow(Image);
-end
-
