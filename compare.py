9cde36e6522bd3112d73bba4baa5b039540e026e
Jamie Connelly <jamiebruceconnelly@gmail.com>
2016-11-02 15:25:35 +0000

    updated compare class

--- compare.py
+++ compare.py
@@ -1,6 +1,7 @@
 import glob
 import cv2
 import os
+from math import sqrt, ceil, floor
 
 
 class Compare:
@@ -12,17 +13,13 @@ class Compare:
                     "Intersection": cv2.HISTCMP_INTERSECT,
                     "Hellinger": cv2.HISTCMP_HELLINGER}
 
-    def __init__(self, img_path):
-        self.org_img_name = img_path[img_path.rfind("/") + 1:]
-        self.orginal_img = cv2.imread(img_path)
-        self.org_img_hist = self.compute_histograms(self.org_img_name,
-                                                    self.orginal_img)
-
-        for imagePath in glob.glob(os.getcwd() +
-                                   "/Images/out_natural_1k/*.jpg"):
-            filename = imagePath[imagePath.rfind("/") + 1:]
-            image = cv2.imread(imagePath)
-            Compare.index[filename] = self.compute_histograms(filename, image)
+    def __init__(self, target_img_path, out_img_dim, num_of_tiles):
+        self.target_img_name = target_img_path[target_img_path.rfind("/") + 1:]
+        self.out_img_dim = out_img_dim
+        self.orginal_img = cv2.imread(target_img_path).resize(out_img_dim[0], out_img_dim[1])
+        self.tile_size = self.get_tile_size(num_of_tiles)
+        self.read_src_images()
+        # self.org_img_hist = self.compute_histograms(self.target_img_name, self.orginal_img)
 
     def compute_histograms(self, filename, image):
         Compare.images[filename] = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -33,6 +30,24 @@ class Compare:
         cv2.normalize(hist, hist)
         hist = hist.flatten()
         return hist
+
+    def read_srs_images(self):
+        for imagePath in glob.glob(os.getcwd() +
+                                   "/Images/out_natural_1k/*.jpg"):
+            filename = imagePath[imagePath.rfind("/") + 1:]
+            image = cv2.imread(imagePath)
+            Compare.index[filename] = self.compute_histograms(filename, image)
+
+    def calc_columns_rows(n):
+        num_columns = int(ceil(sqrt(n)))
+        num_rows = int(ceil(n / float(num_columns)))
+        return (num_columns, num_rows)
+
+    def get_tile_size(self, tiles):
+        w, h = self.orginal_img.size
+        columns, rows = self.calc_columns_rows(tiles)
+        tile_w, tile_h = int(floor(w / columns)), int(floor(h / rows))
+        return tile_w, tile_h
 
     def compare_histograms(self, method):
         results = {}
