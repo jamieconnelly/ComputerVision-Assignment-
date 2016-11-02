e25ab9aaae4dde441c59be9165e70bae27eefb88
frankpuppa <fdellapuppa@dundee.ac.uk>
2016-11-02 21:55:32 +0000

    added choose dir

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from Tkinter import *
 from compare import Compare
-from tkFileDialog import askopenfilename
+from tkFileDialog import askopenfilename, askdirectory
 import numpy as np
-import image_slicer
+#import image_slicer
 
 root = Tk()
 root.minsize(width=400, height=400)
@@ -16,25 +16,30 @@ def start_btn_cb(val):
     # Print first tile as matrix
     # print (np.asarray(tiles[0].image.convert('L')))
     # Show first tile as image
-    x = Compare(image_path, (100, 100), 10)
+    x = Compare(image_path, (100, 100), 10, src_dir)
     #print x.compare_histograms('Correlation')
 
 
 def open_img_btn_cb():
     global image_path
     image_path = askopenfilename()
+def open_dir():
+	global src_dir
+	src_dir=askdirectory()
 
 
 # Define widgets
 tile_no_lbl = Label(root, text="Number of tiles")
 tile_no_val = Entry(root, bd=1)
 open_img_btn = Button(root, text ="Load Image", command=open_img_btn_cb)
+source_dir=Button(root, text ="Choose Source Dir", command=open_dir)
 start_btn = Button(root, text ="Start", command=lambda v=tile_no_val: start_btn_cb(v))
 
 
 # Pack widgets
 start_btn.pack(side=BOTTOM, fill=X)
 open_img_btn.pack(side=BOTTOM, fill=X)
+source_dir.pack(side=TOP,fill=X)
 tile_no_lbl.pack(side=LEFT)
 tile_no_val.pack(side=RIGHT)
 
