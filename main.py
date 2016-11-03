13463dc90a6d3af01a0c91dea6780eaf22104424
Jamie Connelly <jamiebruceconnelly@gmail.com>
2016-11-03 11:40:42 +0000

    - Changed the name of the compare class to Mosaic - Removed hard coded file paths - Updated constructor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from Tkinter import *
-from compare import Compare
+from compare import Mosaic
 from tkFileDialog import askopenfilename, askdirectory
 import numpy as np
 
@@ -10,10 +10,11 @@ src_dir = None
 
 
 # Define widget callbacks
-def start_btn_cb(val):
-    tiles = int(val.get())
-    x = Compare(image_path, (800, 600), 8000, src_dir)
-    x.create_mosaic()
+def start_btn_cb(img_h, img_w):
+    img_h = int(img_h.get())
+    img_w = int(img_w.get())
+    mos = Mosaic(image_path, (img_h, img_w), 10, src_dir, 'Intersection', 'out')
+    mos.create_mosaic()
 
 
 def open_img_btn_cb():
@@ -27,18 +28,22 @@ def open_dir():
 
 
 # Define widgets
-tile_no_lbl = Label(root, text="Number of tiles")
-tile_no_val = Entry(root, bd=1)
+img_w_lbl = Label(root, text="Image Width")
+img_w_val = Entry(root, bd=1)
+img_h_lbl = Label(root, text="Image Height")
+img_h_val = Entry(root, bd=1)
 open_img_btn = Button(root, text ="Load Image", command=open_img_btn_cb)
-source_dir=Button(root, text ="Choose Source Dir", command=open_dir)
-start_btn = Button(root, text ="Start", command=lambda v=tile_no_val: start_btn_cb(v))
+source_dir = Button(root, text ="Choose Source Dir", command=open_dir)
+start_btn = Button(root, text ="Start",
+                   command=lambda w=img_w_val, h=img_h_val: start_btn_cb(w, h))
 
 
 # Pack widgets
 start_btn.pack(side=BOTTOM, fill=X)
 open_img_btn.pack(side=BOTTOM, fill=X)
-source_dir.pack(side=TOP,fill=X)
-tile_no_lbl.pack(side=LEFT)
-tile_no_val.pack(side=RIGHT)
-
+source_dir.pack(side=BOTTOM,fill=X)
+img_w_lbl.pack()
+img_w_val.pack()
+img_h_lbl.pack()
+img_h_val.pack()    
 root.mainloop()
