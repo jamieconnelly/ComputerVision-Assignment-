3f5fe966cf45fd3e7e8e1286b023724a6f30b0ce
frankpuppa <fdellapuppa@dundee.ac.uk>
2016-11-07 14:12:49 +0000

    refactored a little the code for partB

--- partB.py
+++ partB.py
@@ -15,20 +15,22 @@ def compute_histogram(image):
    return hist
 
 
-def read_src_images(src_dir, index, filename_arr):
+def read_src_images(src_dir, index):
     for imagePath in glob.glob(src_dir + "*.jpg"):
-        filename = imagePath[imagePath.rfind("/") + 1:]
         image = cv2.imread(imagePath)
         image = cv2.resize(image, (50, 50))
-        #arr = np.concatenate(image)
         index.append(compute_histogram(image))
-        filename_arr.append(filename)
-        #index[filename] = image
-        #index["matrix"] = image
+
+
+def define_prediction(value):
+    if value[0] == 0:
+        print(str(value), "Natural")
+    elif value[0] ==1:
+        print(str(value), "ManMade")
+
 
 def main():
-    index =[]
-    filename_arr = []
+    index = []
     #image_src = "/home/frank/OpenCV/ComputerVision-Assignment-/ImagesAssignment/Images/natural_test/out_natural_1k/sun_aabzgoowomavupwl.jpg"
     #image_src = "/home/frank/OpenCV/ImagesAssignment/Images/natural_training/out_natural_1k/sun_aachaucttrrkdicr.jpg"
     #image_src ="/home/frank/OpenCV/ImagesAssignment/Images/natural_test/out_natural_1k/sun_aabzgoowomavupwl.jpg"
@@ -37,22 +39,20 @@ def main():
     # image_hist = compute_histogram(image)
     image = cv2.resize(image, (50, 50))
     #arr = np.concatenate(image)
-    arr=compute_histogram(image)
+    arr = compute_histogram(image)
     knn = KNeighborsClassifier(n_neighbors=5)
 
-    read_src_images(manmade_src_dir, index, filename_arr)
-    #r1 = np.full((1, len(index)), 0, dtype=np.int8)
-    r1 = [0] * len(index)
+    read_src_images(manmade_src_dir, index)
+    r1 = [1] * len(index) # Label manmade
     length = len(index)
-    read_src_images(natural_src_dir, index, filename_arr)
-    #r2 = np.full((1, (len(index) - length)), 1, dtype=np.int8)
-    r2 = [1] * (len(index) - length)
-    #responses = np.concatenate((r1, r2), axis=0)
-    responses = r1 + r2
-    print 'finished computing  histograms'
+    read_src_images(natural_src_dir, index)
+    r2 = [0] * (len(index) - length) # Label natural
+
+    responses = r1 + r2 # concatenate labels
+    print 'feeding KNN to make an evaluation'
     knn.fit(index, responses)
 
-    print(knn.predict([arr]))
+    define_prediction(knn.predict([arr])) #print value predicted
 
 if __name__ == '__main__':
     main()
