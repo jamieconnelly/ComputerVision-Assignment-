7903704892f71e74f6011b9c674d9ebc82dd7760
Jamie Connelly <jamiebruceconnelly@gmail.com>
2016-11-18 12:59:23 +0000

    - Added hog, canny and hough feature extractors - Changed SVM to Linear SVM

--- feature_extractor.py
+++ feature_extractor.py
@@ -1,3 +1,4 @@
+from skimage import feature, color, transform
 import cv2
 
 
@@ -7,3 +8,23 @@ def compute_histogram(image):
     cv2.normalize(hist, hist)
     hist = hist.flatten()
     return hist
+
+
+def compute_canny(image):
+    grey_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    edges = feature.canny(grey_image)
+    return edges.flatten()
+
+
+def compute_hog(image):
+    grey_image = color.rgb2gray(image)
+    grey_image = cv2.resize(grey_image, (50, 50), interpolation=cv2.INTER_AREA)
+    hist, hog = feature.hog(grey_image, orientations=8, pixels_per_cell=(16, 16),
+                            cells_per_block=(1, 1), visualise=True)
+    return hog.flatten()
+
+
+def compute_hough(image):
+    grey_image = color.rgb2gray(image)
+    h, theta, distances = transform.hough_line(grey_image)
+    return distances
